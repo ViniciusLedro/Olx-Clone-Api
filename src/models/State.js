37b20e8f714e221b86b6bc50499542f7398c28e5
Vinicius Ledro <ledro.ledro16@gmail.com>
2024-06-12 08:00:07 -0300

    first commit

---
+++ src/models/State.js
@@ -0,0 +1,47 @@
+import { prisma } from "../../config/prisma.js";
+
+
+
+export const createState = async (name) =>{
+    return await prisma.state.create({
+        data : {name}
+    });
+};
+
+
+export const findAllStates = async () => {
+    return await prisma.state.findMany();
+}
+
+
+
+export const findStateById = async () =>{
+    return await prisma.state.findUnique({
+        where : { id: stateId},
+    });
+};
+
+
+
+export const updateState = async(id,data)=>{
+    return await prisma.state.update({
+        where:{id},
+        data:{
+            name:data.name,
+        },
+    });
+};
+
+export const findStateByName = async (stateName) => {
+    return await prisma.state.findUnique({
+        where: {
+            name: stateName,
+        },
+    });
+}
+
+export const deleteState = async(id) =>{
+    return await prisma.state.delete({
+        where:{id},
+    }) 
+}
